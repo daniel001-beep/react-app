0951b67e41354ab33ac5b46b422cb397c540afc7
idowu daniel <idowuisdaniel1@gmail.com>
2025-12-27 00:22:36 +0100

    Add actual React project files

---
+++ react5/src/App.jsx
@@ -0,0 +1,17 @@
+import "./index.css";
+import Quiz from "./components/quiz..jsx";
+import Results from "./components/results.jsx";
+
+
+function App() {
+    return (
+        <div className="app-container">
+            {" "}
+            <h1>Quiz App</h1>
+            <Quiz />
+        </div>
+    )
+}
+
+export default App;
+
